e05199302c87ce22a06be01a46e07a0bbd8ab504
raomartin <martin.rao@hotmail.com>
2023-09-08 10:46:36 -0300

    Initial commit

---
+++ src/universidad/grupo20/AccesoADatos/Conexion.java
@@ -0,0 +1,37 @@
+
+package universidad.grupo20.AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidad-grupo20";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+    
+    private Conexion(){};
+    
+    public static Connection getConexion(){
+        if(connection == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
+                
+                JOptionPane.showMessageDialog(null, "Conectado a la Base de Datos"); //Mensaje de Prueba
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error al cargar los Drivers. "+ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error al conectarse a la Base de Datos. "+ex.getMessage());
+            }
+        }
+        return connection;
+    }
+    
+}
---
+++ src/universidad/grupo20/UniversidadGrupo20.java
@@ -0,0 +1,18 @@
+
+package universidad.grupo20;
+
+import java.sql.Connection;
+import universidad.grupo20.AccesoADatos.Conexion;
+
+public class UniversidadGrupo20 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Connection con = Conexion.getConexion();
+        
+        
+        
+    }
+    
+}

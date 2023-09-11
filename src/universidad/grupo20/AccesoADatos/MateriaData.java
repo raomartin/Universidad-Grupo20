6d0dd5c9275514903913704fde5689c2242e43d3
lucas-ema-mar <141653486+lucas-ema-mar@users.noreply.github.com>
2023-09-11 12:48:57 -0300

    Clases hechas
    Cambios hechos

---
+++ src/universidad/grupo20/AccesoADatos/MateriaData.java
@@ -0,0 +1,212 @@
+
+package universidad.grupo20.AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import universidad.grupo20.Entidades.Alumno;
+import universidad.grupo20.Entidades.Materia;
+
+
+public class MateriaData {
+     private Connection cone=null;
+   
+    //CONSTRUCTOR(INICIALIZAR LA VARIABLE CONE)
+    public MateriaData(){
+        cone=Conexion.getConexion();
+    }
+    
+    
+    public void guardarMateria (Materia materia){
+ 
+      
+        /*UTILIZAMOS EL METODO INSERT E INICIALIZAMOS LOS CAMPOS(DNI,APELLIDO ETC) CON CARACTERES COMODINES(?),
+            PARA REMPLAZARLOS LUEGO*/
+        String sql = "INSERT INTO materia(Nombre,año,estado)"
+                + "VALUES (?,?,?)";
+
+        try {
+            //GENERAMOS OBJETO Y PEDIMOS QUE NOS RETORNE LAS ID GENERADAS AUTOMATICAMENTE(sql,Statement.return_generated_keys)
+            PreparedStatement ps = cone.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            //REMPLAZAMOS LOS COMODINES(?)
+            //UTILIZAMOS VALUEOF(),PARA PODER CONVERTIR A DATE UN LOCALDATE
+            
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3,materia.isEstado());
+            
+            
+            //PEDIMOS LA CLAVE GENERADA AUTOMATICAMENTE(ID ALUMNO)
+            //res.next(),PREGUNTA SI HAY OTRA FILA O APUNTA A LA SIGUIENTE FILA
+            
+            ResultSet res = ps.getGeneratedKeys();
+            if (res.next()) {
+                materia.setIdMateria(res.getInt(1));
+                JOptionPane.showMessageDialog(null, "MATERIA GUARDADA");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA");
+
+        }
+
+    }
+    
+   
+    
+    public Materia buscarMateria(int id){
+        String sql ="SELECT nombre, año FROM materia WHERE idMateria=? AND estado=1";
+        
+      
+        Materia materia=null;
+        try {
+            PreparedStatement ps = cone.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                materia = new Materia();
+
+                materia.setNombre(rs.getString("nombre"));
+
+                materia.setAnio(rs.getInt("año"));
+                materia.setEstado(true);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "NO EXISTE LA MATERIA");
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA");
+        }
+        return materia;
+        
+
+    }
+    
+    
+    
+    
+    
+   
+     
+    
+    
+    
+    
+        
+     public List<Materia> listarMaterias(){
+        
+        String sql="SELECT idMateria,nombre,año FROM materia WHERE estado=1";
+                
+        ArrayList<Materia> materias=new ArrayList<>();
+        
+        
+        try {
+            PreparedStatement ps=cone.prepareStatement(sql);
+           
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                Materia materia=new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+               
+               
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnio(rs.getInt("año"));
+                materia.setEstado(true);
+                
+                materias.add(materia);
+            
+            }
+            ps.close();
+      
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA MATERIA");
+        }
+        return materias;
+    }
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    public void modificarMateria(Materia materia){
+       //INGRESAMOS CODIGO PARA MODIFICAR UN DATO (UPDATE)
+       
+        String sql="UPDATE materia SET nombre=?,año=?,estado=? WHERE idMateria=?";
+       
+       try {
+            //CAMBIAMOS COMODINES(?)
+            PreparedStatement ps=cone.prepareStatement(sql);
+            
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2,materia.getAnio());
+            ps.setBoolean(3,materia.isEstado());
+            ps.setInt(4,materia.getIdMateria());
+           
+            
+            // PREGUNTAMOS SI LOGRO EL ENVIO DE DATOS
+            
+            int exito=ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null,"MATERIA MODIFICADA");
+            }
+            
+                    
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA MATERIA");
+           
+        }
+        
+        
+    }
+    
+    
+    public void borrarMateria(int id){
+        
+        //CREAMOS CODIGO PARA MODIFICAR EL "ESTADO"DE UN ALUMNO(TAMBIEN SE PUEDE ELIMINAR
+        //PERO NO ES LO PEDIDO
+        String sql = "UPDATE materia SET estado=0 WHERE idMateria=?";
+        
+        
+        try {
+
+            PreparedStatement ps = cone.prepareStatement(sql);
+            ps.setInt(1,id);
+            int exito =ps.executeUpdate();
+            if(exito==1){
+               JOptionPane.showMessageDialog(null,"MATERIA DADA DE BAJA");
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ERROR AL DAR DE BAJA LA MATERIA");
+        }
+        
+        
+        
+    }
+    
+}
--- src/universidad/grupo20/UniversidadGrupo20.java
+++ src/universidad/grupo20/UniversidadGrupo20.java
@@ -2,17 +2,58 @@
 package universidad.grupo20;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.Month;
+import universidad.grupo20.AccesoADatos.AlumnoData;
 import universidad.grupo20.AccesoADatos.Conexion;
+import universidad.grupo20.Entidades.Alumno;
 
 public class UniversidadGrupo20 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Connection con = Conexion.getConexion();
-        
-        
-        
+        AlumnoData alu = new AlumnoData();
+//        Alumno Juan = new Alumno(42273781, "Rodríguez", "Juan", LocalDate.of(1998, 4, 25), true);
+//        Alumno Pedro = new Alumno(387626909, "Perez", "Pedro", LocalDate.of(1996, 7, 10), true);
+//        Alumno Esteban = new Alumno(298426909, "Quito", "Esteban", LocalDate.of(1998, 10, 1), true);
+//        Alumno Monica = new Alumno(407626909, "Galindo", "Monica", LocalDate.of(2000, 3, 19), true);
+//        Alumno Armando = new Alumno(44626909, "Paredes", "Armando", LocalDate.of(1999, 8, 5), true);
+//
+//            Alumno Armando = new Alumno(1, 44626909,"Paredes", "Armando", LocalDate.of(1999, 8, 5), false);
+//               alu.modificarAlumno(Armando);
+//        alu.guardarAlumno(Armando);
+//        alu.guardarAlumno(Juan);
+//        alu.guardarAlumno(Pedro);
+//        alu.guardarAlumno(Esteban);
+//        alu.guardarAlumno(Monica);
+//        
+//            Alumno alumnoEncontrado=alu.buscarAlumno(2);
+//            System.out.println("ID "+alumnoEncontrado.getIdAlumno());
+//            System.out.println("Apellido "+alumnoEncontrado.getApellido());
+//            System.out.println("Nombre "+alumnoEncontrado.getNombre()); 
+//            System.out.println("DNI "+alumnoEncontrado.getDni());
+//            System.out.println("Fecha nacimineto "+alumnoEncontrado.getFechaNac());
+//            System.out.println("Estado "+alumnoEncontrado.isEstado());
+            
+
+//
+//            Alumno buscadoporDni=alu.buscarAlumnoPorDni(298426909);
+//            System.out.println("Alumno encontado por DNI "+buscadoporDni.getDni());
+//            System.out.println("ID"+buscadoporDni.getIdAlumno());
+//            System.out.println("Apellido "+buscadoporDni.getApellido());
+//            System.out.println("Nombre "+buscadoporDni.getNombre());
+//            System.out.println("Fecha de nacimiento"+buscadoporDni.getFechaNac() );
+//            System.out.println("Estado "+buscadoporDni.isEstado());
+
+                
+              
+//            alu.borrarAlumno(3);
+           
+           for(Alumno alumno:alu.listarAlumnos()){
+               System.out.println("ID "+alumno.getIdAlumno()+ "Nombre "+alumno.getNombre()+ "Apellido"+alumno.getApellido());
+                       
     }
-    
+    }
 }

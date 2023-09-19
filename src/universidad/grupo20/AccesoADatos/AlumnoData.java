3422b51e2dfafe6c1c16bdb067ede73b273d37ee
nahuel <nahuelf.nr@gmail.com>
2023-09-19 10:37:33 -0300

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 339d19075d44ba06d577264bcb409a8118204e8d.

--- src/universidad/grupo20/AccesoADatos/AlumnoData.java
+++ src/universidad/grupo20/AccesoADatos/AlumnoData.java
@@ -1,6 +1,8 @@
+
 package universidad.grupo20.AccesoADatos;
 
 //FABRIZZIO
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -14,59 +16,68 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import universidad.grupo20.Entidades.Alumno;
 
+
 public class AlumnoData {
-
-    private Connection cone = null;
-
+    private Connection cone=null;
+   
     //CONSTRUCTOR(INICIALIZAR LA VARIABLE CONE)
-    public AlumnoData() {
-        cone = Conexion.getConexion();
+    public AlumnoData(){
+        cone=Conexion.getConexion();
     }
-
-    public void guardarAlumno(Alumno alumno) {
-
+    
+    
+    public void guardarAlumno (Alumno alumno){
+ 
+      
         /*UTILIZAMOS EL METODO INSERT E INICIALIZAMOS LOS CAMPOS(DNI,APELLIDO ETC) CON CARACTERES COMODINES(?),
             PARA REMPLAZARLOS LUEGO*/
         String sql = "INSERT INTO alumno(dni,apellido,nombre,fechaNac,estado)"
                 + "VALUES (?,?,?,?,?)";
+
         try {
             //GENERAMOS OBJETO Y PEDIMOS QUE NOS RETORNE LAS ID GENERADAS AUTOMATICAMENTE(sql,Statement.return_generated_keys)
             PreparedStatement a1 = cone.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
+            
             //REMPLAZAMOS LOS COMODINES(?)
             //UTILIZAMOS VALUEOF(),PARA PODER CONVERTIR A DATE UN LOCALDATE
-            a1.setInt(1, alumno.getDni());
+            
+            a1.setInt(1,alumno.getDni());
             a1.setString(2, alumno.getApellido());
-            a1.setString(3, alumno.getNombre());
-            a1.setDate(4, Date.valueOf(alumno.getFechaNac()));
-            a1.setBoolean(5, alumno.isEstado());
+            a1.setString(3,alumno.getNombre());
+            a1.setDate(4,Date.valueOf(alumno.getFechaNac()));
+            a1.setBoolean(5,alumno.isEstado());
             a1.executeUpdate();
-
+            
             //PEDIMOS LA CLAVE GENERADA AUTOMATICAMENTE(ID ALUMNO)
             //res.next(),PREGUNTA SI HAY OTRA FILA O APUNTA A LA SIGUIENTE FILA
+            
             ResultSet res = a1.getGeneratedKeys();
             if (res.next()) {
                 alumno.setIdAlumno(res.getInt(1));
                 JOptionPane.showMessageDialog(null, "ALUMNO GUARDADO");
             }
             a1.close();
+
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO");
+
         }
+
     }
-
-    public Alumno buscarAlumno(int id) {
-
-        String sql = "SELECT dni,apellido,nombre,fechaNac FROM alumno WHERE idAlumno=? AND estado=1";
-
-        //variable Alumno
-        Alumno alumno = null;
-        try {
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                alumno = new Alumno();
+    
+    //me quede aca 08/09
+    
+    public Alumno buscarAlumno(int id){
+        String sql ="SELECT dni,apellido,nombre,fechaNac FROM alumno WHERE idAlumno=? AND estado=1";
+        
+        //variable alumno
+        Alumno alumno=null;
+   try {
+            PreparedStatement ps=cone.prepareStatement(sql);
+            ps.setInt(1,id);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                alumno=new Alumno();
                 alumno.setIdAlumno(id);
                 alumno.setDni(rs.getInt("dni"));
                 alumno.setApellido(rs.getString("apellido"));
@@ -74,114 +85,164 @@ public class AlumnoData {
                 //USAMOS TOLOCALDATE PARA CONVERTIR UN LOCALDATE A DATE
                 alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
                 alumno.setEstado(true);
-
-            } else {
-                JOptionPane.showMessageDialog(null, "NO EXISTE EL ALUMNO");
-
+                
+            }else{
+                JOptionPane.showMessageDialog(null,"NO EXISTE EL ALUMNO");
+            
             }
+   
             ps.close();
+            
+          
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA ALUMNO");
         }
-        return alumno;
+   return alumno;
+        
+
     }
-
-    public Alumno buscarAlumnoPorDni(int dni) {
-
-        String sql = "SELECT idAlumno,apellido,nombre,fechaNac FROM alumno WHERE dni=? AND estado=1";
-        Alumno alumno = null;
+    
+    
+    
+    
+    
+    public Alumno buscarAlumnoPorDni(int dni){
+        
+        String sql="SELECT idAlumno,apellido,nombre,fechaNac FROM alumno WHERE dni=? AND estado=1";
+        Alumno alumno=null;
+        
         try {
-            PreparedStatement ps = cone.prepareStatement(sql);
+            PreparedStatement ps=cone.prepareStatement(sql);
             ps.setInt(1, dni);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                alumno = new Alumno();
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                alumno=new Alumno();
                 alumno.setIdAlumno(rs.getInt("idAlumno"));
                 alumno.setDni(dni);
                 alumno.setApellido(rs.getString("apellido"));
                 alumno.setNombre(rs.getString("nombre"));
                 alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
                 alumno.setEstado(true);
-            } else {
-                JOptionPane.showMessageDialog(null, "NO EXISTE ESE ALUMNO");
+            }else{
+                JOptionPane.showMessageDialog(null,"NO EXISTE ESE ALUMNO");
             }
             ps.close();
+      
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA ALUMNO");
         }
         return alumno;
     }
-
-    public List<Alumno> listarAlumnos() {
-
-        String sql = "SELECT idAlumno,dni,apellido,nombre,fechaNac FROM alumno WHERE estado=1";
-        ArrayList<Alumno> alumnos = new ArrayList<>();
-
+    
+    
+    
+    
+        
+     public List<Alumno> listarAlumnos(){
+        
+        String sql="SELECT idAlumno,dni,apellido,nombre,fechaNac FROM alumno WHERE estado=1";
+                
+        ArrayList<Alumno> alumnos=new ArrayList<>();
+        
+        
         try {
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Alumno alumno = new Alumno();
+            PreparedStatement ps=cone.prepareStatement(sql);
+           
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                Alumno alumno=new Alumno();
                 alumno.setIdAlumno(rs.getInt("idAlumno"));
                 alumno.setDni(rs.getInt("dni"));
                 alumno.setApellido(rs.getString("apellido"));
                 alumno.setNombre(rs.getString("nombre"));
                 alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
                 alumno.setEstado(true);
-
+                
                 alumnos.add(alumno);
+            
             }
-            JOptionPane.showMessageDialog(null, "ALUMNOS LISTADOS");
             ps.close();
+      
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA ALUMNO");
         }
         return alumnos;
     }
-
-    public void modificarAlumno(Alumno alumno) {
-
-        //INGRESAMOS CODIGO PARA MODIFICAR UN DATO (UPDATE)
-        String sql = "UPDATE alumno SET dni=?,apellido=?,nombre=?,fechaNac=?,estado=? WHERE idAlumno=?";
-
-        try {
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    public void modificarAlumno(Alumno alumno){
+       //INGRESAMOS CODIGO PARA MODIFICAR UN DATO (UPDATE)
+       
+        String sql="UPDATE alumno SET dni=?,apellido=?,nombre=?,fechaNac=?,estado=? WHERE idAlumno=?";
+       
+       try {
             //CAMBIAMOS COMODINES(?)
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ps.setInt(1, alumno.getDni());
-            ps.setString(2, alumno.getApellido());
-            ps.setString(3, alumno.getNombre());
-            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
-            ps.setBoolean(5, alumno.isEstado());
-            ps.setInt(6, alumno.getIdAlumno());
-
+            PreparedStatement ps=cone.prepareStatement(sql);
+            ps.setInt(1,alumno.getDni());
+            ps.setString(2,alumno.getApellido());
+            ps.setString(3,alumno.getNombre());
+            ps.setDate(4,Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5,alumno.isEstado());
+            ps.setInt(6,alumno.getIdAlumno());
+            
             // PREGUNTAMOS SI LOGRO EL ENVIO DE DATOS
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "ALUMNO MODIFICADO");
+            
+            int exito=ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null,"ALUMNO MODIFICADO");
             }
-            ps.close();
+            
+                    
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCER A LA TABLA ALUMNOS. " + ex.getMessage());
+           JOptionPane.showMessageDialog(null,"ERROR AL ACCER A LA TABLA ALUMNOS");
+           
         }
+        
+        
     }
-
-    public void borrarAlumno(int id) {
-
-        //CREAMOS CODIGO PARA MODIFICAR EL "ESTADO" DE UN ALUMNO(TAMBIEN SE PUEDE ELIMINAR DE LA BASE)
-        //PERO LO QUE PIDE ES UN BORRADO LOGICO Y NO FISICO
+    
+    
+    public void borrarAlumno(int id){
+        
+        //CREAMOS CODIGO PARA MODIFICAR EL "ESTADO"DE UN ALUMNO(TAMBIEN SE PUEDE ELIMINAR
+        //PERO NO ES LO PEDIDO
         String sql = "UPDATE alumno SET estado=0 WHERE idAlumno=?";
-
+        
+        
         try {
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ps.setInt(1, id);
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "ALUMNO DADO DE BAJA");
-            }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL DAR DE BAJA AL ALUMNO. " + ex.getMessage());
-        }
-    }
 
+            PreparedStatement ps = cone.prepareStatement(sql);
+            ps.setInt(1,id);
+            int exito =ps.executeUpdate();
+            if(exito==1){
+               JOptionPane.showMessageDialog(null,"ALUMNO DADO DE BAJA");
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ERROR AL DAR DE BAJA AL ALUMNO");
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
 }
--- src/universidad/grupo20/AccesoADatos/MateriaData.java
+++ src/universidad/grupo20/AccesoADatos/MateriaData.java
@@ -1,3 +1,4 @@
+
 package universidad.grupo20.AccesoADatos;
 
 import java.sql.Connection;
@@ -12,138 +13,200 @@ import javax.swing.JOptionPane;
 import universidad.grupo20.Entidades.Alumno;
 import universidad.grupo20.Entidades.Materia;
 
+
 public class MateriaData {
-
-    private Connection cone = null;
-
+     private Connection cone=null;
+   
     //CONSTRUCTOR(INICIALIZAR LA VARIABLE CONE)
-    public MateriaData() {
-        cone = Conexion.getConexion();
+    public MateriaData(){
+        cone=Conexion.getConexion();
     }
-
-    public void guardarMateria(Materia materia) {
-
-        /*UTILIZAMOS EL METODO INSERT E INICIALIZAMOS LOS CAMPOS(NOMBRE,ESTADO Y ANIO) CON CARACTERES COMODINES(?),
+    
+    
+    public void guardarMateria (Materia materia){
+ 
+      
+        /*UTILIZAMOS EL METODO INSERT E INICIALIZAMOS LOS CAMPOS(DNI,APELLIDO ETC) CON CARACTERES COMODINES(?),
             PARA REMPLAZARLOS LUEGO*/
-        String sql = "INSERT INTO materia(Nombre,año,estado)"
-                + "VALUES (?,?,?)";
+        String sql = "INSERT INTO materia(Nombre,año,estado)VALUES (?,?,?)";
 
         try {
             //GENERAMOS OBJETO Y PEDIMOS QUE NOS RETORNE LAS ID GENERADAS AUTOMATICAMENTE(sql,Statement.return_generated_keys)
             PreparedStatement ps = cone.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
+            
             //REMPLAZAMOS LOS COMODINES(?)
-            ps.setString(1, materia.getNombre());
+            //UTILIZAMOS VALUEOF(),PARA PODER CONVERTIR A DATE UN LOCALDATE
+            
+            ps.setString(1,materia.getNombre());
             ps.setInt(2, materia.getAnio());
-            ps.setBoolean(3, materia.isEstado());
+            ps.setBoolean(3,materia.isEstado());
             ps.executeUpdate();
-
-            //PEDIMOS LA CLAVE GENERADA AUTOMATICAMENTE(ID MATERIA)
+            
+            
+            //PEDIMOS LA CLAVE GENERADA AUTOMATICAMENTE(ID ALUMNO)
             //res.next(),PREGUNTA SI HAY OTRA FILA O APUNTA A LA SIGUIENTE FILA
+            
             ResultSet res = ps.getGeneratedKeys();
             if (res.next()) {
                 materia.setIdMateria(res.getInt(1));
                 JOptionPane.showMessageDialog(null, "MATERIA GUARDADA");
             }
             ps.close();
+
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA");
+
         }
+
     }
-
-    public Materia buscarMateria(int id) {
-
-        String sql = "SELECT nombre, año FROM materia WHERE idMateria=? AND estado=1";
-
-        //Variable Materia
-        Materia materia = null;
+    
+   
+    
+    public Materia buscarMateria(int id){
+        String sql ="SELECT nombre, año FROM materia WHERE idMateria=? AND estado=1";
+        
+      
+        Materia materia=null;
         try {
             PreparedStatement ps = cone.prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 materia = new Materia();
-                materia.setIdMateria(id);
+
                 materia.setNombre(rs.getString("nombre"));
+
                 materia.setAnio(rs.getInt("año"));
                 materia.setEstado(true);
 
             } else {
                 JOptionPane.showMessageDialog(null, "NO EXISTE LA MATERIA");
+
             }
+
             ps.close();
+
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA");
         }
         return materia;
+        
+
     }
-
-    public List<Materia> listarMaterias() {
-
-        String sql = "SELECT idMateria,nombre,año FROM materia WHERE estado=1";
-        ArrayList<Materia> materias = new ArrayList<>();
-
+    
+    
+    
+    
+    
+   
+     
+    
+    
+    
+    
+        
+     public List<Materia> listarMaterias(){
+        
+        String sql="SELECT idMateria,nombre,año FROM materia WHERE estado=1";
+                
+        ArrayList<Materia> materias=new ArrayList<>();
+        
+        
         try {
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Materia materia = new Materia();
+            PreparedStatement ps=cone.prepareStatement(sql);
+           
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                Materia materia=new Materia();
                 materia.setIdMateria(rs.getInt("idMateria"));
+               
+               
                 materia.setNombre(rs.getString("nombre"));
                 materia.setAnio(rs.getInt("año"));
                 materia.setEstado(true);
-
+                
                 materias.add(materia);
+            
             }
-            JOptionPane.showMessageDialog(null, "MATERIAS LISTADAS");
             ps.close();
+      
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA. " + ex.getMessage());
+            JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA MATERIA");
         }
         return materias;
     }
-
-    public void modificarMateria(Materia materia) {
-
-        //INGRESAMOS CODIGO PARA MODIFICAR UN DATO (UPDATE)
-        String sql = "UPDATE materia SET nombre=?,año=?,estado=? WHERE idMateria=?";
-
-        try {
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    public void modificarMateria(Materia materia){
+       //INGRESAMOS CODIGO PARA MODIFICAR UN DATO (UPDATE)
+       
+        String sql="UPDATE materia SET nombre=?,año=?,estado=? WHERE idMateria=?";
+       
+       try {
             //CAMBIAMOS COMODINES(?)
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ps.setString(1, materia.getNombre());
-            ps.setInt(2, materia.getAnio());
-            ps.setBoolean(3, materia.isEstado());
-            ps.setInt(4, materia.getIdMateria());
-
+            PreparedStatement ps=cone.prepareStatement(sql);
+            
+            ps.setString(1,materia.getNombre());
+            ps.setInt(2,materia.getAnio());
+            ps.setBoolean(3,materia.isEstado());
+            ps.setInt(4,materia.getIdMateria());
+           
+            
             // PREGUNTAMOS SI LOGRO EL ENVIO DE DATOS
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "MATERIA MODIFICADA");
+            
+            int exito=ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null,"MATERIA MODIFICADA");
             }
-            ps.close();
+            
+                    
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA MATERIA. " + ex.getMessage());
+           JOptionPane.showMessageDialog(null,"ERROR AL ACCEDER A LA TABLA MATERIA");
+           
         }
+        
+        
     }
-
-    public void borrarMateria(int id) {
-
-        //CREAMOS CODIGO PARA MODIFICAR EL "ESTADO" DE UNA MATERIA (TAMBIEN SE PUEDE ELIMINAR DE LA BASE)
-        //PERO LO QUE PIDE ES UN BORRADO LOGICO Y NO FISICO
+    
+    
+    public void borrarMateria(int id){
+        
+        //CREAMOS CODIGO PARA MODIFICAR EL "ESTADO"DE UN ALUMNO(TAMBIEN SE PUEDE ELIMINAR
+        //PERO NO ES LO PEDIDO
         String sql = "UPDATE materia SET estado=0 WHERE idMateria=?";
-
+        
+        
         try {
-            PreparedStatement ps = cone.prepareStatement(sql);
-            ps.setInt(1, id);
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "MATERIA DADA DE BAJA");
-            }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL DAR DE BAJA LA MATERIA. " + ex.getMessage());
-        }
-    }
 
+            PreparedStatement ps = cone.prepareStatement(sql);
+            ps.setInt(1,id);
+            int exito =ps.executeUpdate();
+            if(exito==1){
+               JOptionPane.showMessageDialog(null,"MATERIA DADA DE BAJA");
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ERROR AL DAR DE BAJA LA MATERIA");
+        }
+        
+        
+        
+    }
+    
 }
